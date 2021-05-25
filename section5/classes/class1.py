ef4099430cdef554dc113adb92288209d9bf4e18
Maykon D Granemann <maykon.granemann@olist.com>
2021-05-24 22:08:09 -0300

    Aula arquivos texto

--- section5/classes/class1.py
+++ section5/classes/class1.py
@@ -1,13 +1,39 @@
+# Criar arquivo novo 
+# para criar um novo - argumento 'x'
+# arquivo = open('arquivo.txt', 'x')
+# arquivo.write('Escrevendo dentro do arquivo')
+# arquivo.close()
 
-# Criar arquivo novo
-
-# Recricar o conteudo
+# Recricar o conteudo ('w')
+# Se o arquivo nao existir, ele cria
+# arquivo = open('arquivo2.txt', 'w')
+# arquivo.write('Escrevendo dentro do arquivo existente')
+# arquivo.close()
 
 # Adicionar conteudo ao final
+arquivo = open('arquivo3.txt', 'a')
+arquivo.write('Escrevendo no final do arquivo\n')
+arquivo.close()
 
-# Ler um arquivo
+
+# Ler um arquivo, ('r')
+# com este argumento podemos apenas ler o arquivo
+# e um arquivo em modo leitura transforma cada linha em um item de uma lista
+arquivo = open('arquivo3.txt', 'r')
+for linha in arquivo:
+    print(linha)
+arquivo.close()
+
+print('---- limpa -----')
 
 # limpar conteudo do arquivo
+# a funcao strip() em uma string remove os espacos em branco do comece e final
+#    e tambem remove todos os caracateres de escape \n \t \r
+arquivo = open('arquivo3.txt', 'r')
+for linha in arquivo:
+    linha_limpa = linha.strip()
+    print(linha_limpa)
+arquivo.close()
 
 # Ler dados em csv
 
